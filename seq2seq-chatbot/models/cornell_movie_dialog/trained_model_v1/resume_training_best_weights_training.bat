62ec974d916d5722dedb1f3488790c78c334f668
Manish Gujar <shivangi.gujar@gmail.com>
2021-07-20 19:47:48 +0200

    Revert "changes"
    This reverts commit b03344bd6900fdbf9f581037a47b8d72a1628740.

--- seq2seq-chatbot/models/cornell_movie_dialog/trained_model_v1/resume_training_best_weights_training.bat
+++
@@ -1,5 +0,0 @@
-call C:\ProgramData\Anaconda3\scripts\activate.bat C:\ProgramData\Anaconda3
-cd ..\..\..
-python train.py --checkpointfile="models\cornell_movie_dialog\trained_model_v1\best_weights_training.ckpt"
-
-cmd /k
